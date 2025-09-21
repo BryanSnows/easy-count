6713585ef5d4d0c5f22a9ae27f84e89297be6afe
Bryan Neves <bryan.neves@indt.org.br>
2025-09-21 18:50:20 -0400

    feat: add project

---
+++ src/main/java/com/example/easy_count/EasyCountApplication.java
@@ -0,0 +1,13 @@
+package com.example.easy_count;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyCountApplication.class, args);
+	}
+
+}
